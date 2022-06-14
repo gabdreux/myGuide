6be289963af8f9117f8d81bf886665bd99756b30
gabriel dreux <dreuxgabriel@hotmail.com>
2022-06-14 18:58:31 -0300

    first commit

---
+++ src/functions.js
@@ -0,0 +1,10 @@
+// Open and Clone Mobile Menu
+function openNav() {
+    document.getElementById("mobile_menu").style.width = "100%"
+}
+
+function closeNav() {
+    document.getElementById("mobile_menu").style.width = "0"
+}
+
+// ends here
